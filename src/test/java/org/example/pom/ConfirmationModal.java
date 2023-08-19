0567d0e44bbde5b22e8d16b0803cfbbd4c21b427
alewickiy <lewickiy.a@yandex.ru>
2023-08-19 21:02:53 +0300

    autotests.

--- src/test/java/org/example/pom/ConfirmationModal.java
+++ src/test/java/org/example/pom/ConfirmationModal.java
@@ -2,7 +2,6 @@ package org.example.pom;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class ConfirmationModal {
     private final WebDriver WEB_DRIVER;
